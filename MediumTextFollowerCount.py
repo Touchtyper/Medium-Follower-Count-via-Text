a8c65332c54f6c8039913128f941c30b3e1d18a0
Henry Dowding <henrydowding@gmail.com>
2017-08-11 16:20:23 -0500

    Initial Commit

---
+++ MediumTextFollowerCount.py
@@ -0,0 +1,58 @@
+# Python3
+
+# import libraries
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+from twilio.rest import Client
+
+# Twilio Account SID
+
+account_sid = "ACCOUNTSID"
+
+# Twilio Auth Token
+
+auth_token = "AUTHTOKEN"
+
+# Creating client variable 
+
+client = Client(account_sid, auth_token)
+
+# Requests a page listing a specific tag. 
+
+req = Request('https://medium.com/[publication slug]/tagged/[Tag]', headers={'User-Agent': 'Mozilla/5.0'})
+
+# Reads into variable parsepage
+
+parsepage = urlopen(req).read()
+
+# Parses data to variable page
+
+page = BeautifulSoup(parsepage, 'html.parser')
+
+# Searches for class containing follower number
+
+pagetext = page.find('div', attrs={'class': 'u-fontSize14 u-lineHeightBaseSans u-textColorDark u-paddingBottom15'})
+
+# Removes everything but the text
+
+followerstext = pagetext.text.strip()
+
+# Removes the word "Followers"
+
+followers = followerstext.replace('Followers', '')
+
+# Creates and sends a SMS containing the number of Medium followers
+
+message = client.messages.create(
+	to="+15555557896", 
+	from_="+15555551987", # Twilio Number
+	body="You currently have " + followers + " followers on Medium for [Insert Publication Name] publication!")
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,15 @@
+# Medium Follower Count via Text
+
+## Requirements 
+
+- Python3
+- Twilio
+- BeautifulSoup
+
+## Description 
+
+A simple script to scrape the number of followers of a specific publication from Medium.com and then send the result via SMS using Twilio.
+
+
+
+
